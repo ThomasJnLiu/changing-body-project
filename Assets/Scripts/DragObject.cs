9e49aa366f739292830036b570c9150ea6a18010
Thomas <thomasjnliu@gmail.com>
2020-02-20 16:23:14 -0500

    Add drag + dropping with mouse

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Vector3 mOffset;
+
+    private float mZCoord;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnMouseDown(){
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+    void OnMouseDrag(){
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+
+    private Vector3 GetMouseWorldPos(){
+        //pixel coordinates
+        Vector3 mousePoint = Input.mousePosition;
+
+        // z coordinate of game object
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
