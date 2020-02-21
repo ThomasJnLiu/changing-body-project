c372c502668f6c8128527bc11055ae0f2901d189
Thomas <thomasjnliu@gmail.com>
2020-02-21 01:14:28 -0500

    Added UI sliders

--- Assets/Scripts/ClampName.cs
+++ Assets/Scripts/ClampName.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class ClampName : MonoBehaviour
 {
+    public DragObject dragobject;
     public Text drawingLabel, musicLabel, prayerLabel;
     public Slider drawingSlider, musicSlider, prayerSlider;
     // Start is called before the first frame update
@@ -20,5 +21,13 @@ public class ClampName : MonoBehaviour
         musicLabel.transform.position = namePos + new Vector3(0, 60f, 0f);
         drawingLabel.transform.position = namePos + new Vector3(0, 40f, 0f);
         prayerLabel.transform.position = namePos + new Vector3(0, 20f, 0f);
+
+        drawingSlider.transform.position = namePos + new Vector3(70f, 50f, 0f);
+        musicSlider.transform.position = namePos + new Vector3(70f, 70f, 0f);
+        prayerSlider.transform.position = namePos + new Vector3(70f, 30f, 0f);
+
+        drawingSlider.value = dragobject.drawingBar/100;
+        musicSlider.value = dragobject.violinBar/100;
+        prayerSlider.value = dragobject.prayerBar/100;
     }
 }
